ce2e9bdfe76937ffa03d41896714ba5efd44cd0e
HoangNH <hoangnhonline@gmail.com>
2018-06-04 22:34:51 +0700

    .

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -23,6 +23,6 @@ class Banner extends Model  {
      *
      * @var array
      */
-    protected $fillable = ['image_url', 'ads_url', 'time_start', 'time_end', 'object_id', 'object_type', 'type', 'display_order', 'status', 'created_user', 'updated_user'];
+    protected $fillable = ['image_url', 'ads_url', 'time_start', 'time_end', 'object_id', 'object_type', 'type', 'display_order', 'status', 'created_user', 'updated_user', 'status'];
     
 }
